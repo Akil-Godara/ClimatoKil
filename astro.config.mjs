3735958fe25fef4507016545b4d44913a0ba330c
Akil Godara <akeelgodara63@gmail.com>
2026-06-25 18:21:26 +0530

    Remove Cloudflare adapter for static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [],
-  output: 'static',  // or 'hybrid' if using server features
-  adapter: cloudflare(),
-  base: '/',  // Important! Make sure this is '/'
+  output: 'static',
 });
